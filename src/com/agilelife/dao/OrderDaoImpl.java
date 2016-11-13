5c0fa3c3b31087e6e7253b92728d9c250b8cfe45
Alejosd <alejosd5@gmail.com>
2016-11-13 18:54:44 -0300

    1. implementacion de BussinessObject (BO) y DataTransferObject (DTO) para una orden de compra.

--- src/com/agilelife/dao/OrderDaoImpl.java
+++ src/com/agilelife/dao/OrderDaoImpl.java
@@ -1,5 +1,33 @@
 package com.agilelife.dao;
 
-public class OrderDaoImpl {
+import java.sql.SQLException;
+
+import com.agilelife.dto.Order;
+
+public class OrderDaoImpl implements IOrderDao {
+
+	@Override
+	public int crearOrden(Order order) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Order obtenerOrden(Order order) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int actualizarOrden() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int eliminarOrden() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
